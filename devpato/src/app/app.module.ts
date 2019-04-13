6ac10f71f858e0646b730515e66a31645b3853e8
pevargasg <wearetamo@gmail.com>
2019-04-13 16:01:52 -0700

    intro component

--- devpato/src/app/app.module.ts
+++ devpato/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
