b2eae31a2011659a57d95364b6db4eb5dcc3a3d7
pevargasg <wearetamo@gmail.com>
2019-04-13 16:02:18 -0700

    about component

--- devpato/src/app/app.module.ts
+++ devpato/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IntroComponent
+    IntroComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
