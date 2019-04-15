269ff29fd3401bf37b863a30331bda1629dadf98
pevargasg <wearetamo@gmail.com>
2019-04-15 00:00:59 -0700

    rocket launch

--- devpato/src/app/skills/skills.component.ts
+++ devpato/src/app/skills/skills.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.scss"]
 })
 export class SkillsComponent implements OnInit {
+  launch = false;
+  counter = 10;
+  interval: any;
+  start = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  launchRocket() {
+    this.start = true;
+    this.interval = setInterval(_ => {
+      if (this.counter === 0) {
+        clearInterval(this.interval);
+        setTimeout(_ => {
+          this.launch = true;
+          setTimeout(_ => {
+            this.start = false;
+            this.launch = false;
+            this.counter = 10;
+          }, 5000);
+        }, 0);
+      } else {
+        this.counter--;
+      }
+    }, 100);
   }
-
 }
