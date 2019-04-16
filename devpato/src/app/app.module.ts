c70ee3dc3be482da3d5197c0f26fe95f887e3db3
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-04-16 01:35:16 -0400

    code clean up

--- devpato/src/app/app.module.ts
+++ devpato/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { IntroComponent } from "./intro/intro.component";
-import { AboutComponent } from "./about/about.component";
-import { SkillsComponent } from "./skills/skills.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { ContactComponent } from "./contact/contact.component";
-import { ExpirienceComponent } from "./expirience/expirience.component";
-import { SpaceComponent } from "./space/space.component";
-import { MarsComponent } from "./mars/mars.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExpirienceComponent } from './expirience/expirience.component';
+import { SpaceComponent } from './space/space.component';
+import { MarsComponent } from './mars/mars.component';
 
 @NgModule({
   declarations: [
